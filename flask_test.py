dca7ec80e32f1eb665407af81205d522569674d2
Michael <152762704+michaelcjames93@users.noreply.github.com>
2025-02-20 07:16:45 -0500

    Add files via upload

---
+++ flask_test.py
@@ -0,0 +1,15 @@
+from Flask import Flask, request, jsonify
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Flask app is running on Render!"
+
+@app.route('/process', methods=['POST'])
+def process_text():
+    data = request.json
+    user_input = data.get("text", "")
+    return jsonify({"message": f"You entered: {user_input}"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
